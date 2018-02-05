56c67581263ba6c5e9d37703957d227d337ad728
david <david@DESKTOP-AFUGLQ2.sduhsd.lan>
2018-02-05 13:59:28 -0800

    2/5/18

---
+++ APCSA/src/Unit1/Lab01a.java
@@ -0,0 +1,20 @@
+package Unit1;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab01a
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes stars = new StarsAndStripes();
+      stars.printTwoBlankLines();
+      //call the methods needed to make the patterns on the word document
+   }
+}
---
+++ APCSA/src/Unit1/StarsAndStripes.java
@@ -0,0 +1,42 @@
+package Unit1;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   
+   }
+
+   public void printTwentyDashes()
+   {
+	   
+   }
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println();
+	   System.out.println();
+   }
+   
+   public void printASmallBox()
+   {	
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
