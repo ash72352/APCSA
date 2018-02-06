32096f56155e0d2bb729a6ddb0dabe6660c88e71
david <david@DESKTOP-AFUGLQ2.sduhsd.lan>
2018-02-06 14:05:34 -0800

    Unit 2 2/6/18

---
+++ APCSA/src/Unit2/Circle.java
@@ -0,0 +1,25 @@
+package Unit2;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Circle
+{
+	private double radius;
+	private double area;
+
+	public void setRadius(double rad)
+	{
+	}
+
+	public void calculateArea( )
+	{
+	}
+
+	public void print( )
+	{
+	}
+}
