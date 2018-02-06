001546943302090da03f0e5a8c8c1364749c3a2e
david <david@DESKTOP-AFUGLQ2.sduhsd.lan>
2018-02-06 14:13:18 -0800

    Unit 3 2/6/18

---
+++ APCSA/src/Unit3/Lab03c.java
@@ -0,0 +1,24 @@
+package Unit3;
+
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Lab03c
+{
+	public static void main( String[] args )
+ {
+ 	//add test cases
+ 	
+ 	
+		
+		
+	}
+}
