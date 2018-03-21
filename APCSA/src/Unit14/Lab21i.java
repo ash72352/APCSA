1877e1936101cb23450824a22eee385d6d44c103
david <david@DESKTOP-AFUGLQ2.sduhsd.lan>
2018-03-21 14:54:53 -0700

    Lab21i 3/21/18

--- APCSA/src/Unit14/Lab21i.java
+++ APCSA/src/Unit14/Lab21i.java
@@ -16,5 +16,11 @@ public class Lab21i
 {
 	public static void main( String args[] ) throws IOException
 	{
+		Scanner k = new Scanner(new File(System.getProperty("user.dir") + "\\src\\Unit14\\" + "lab21i.dat"));
+		int size = k.nextInt();
+		k.nextLine();
+		String line = k.nextLine();
+		Maze test = new Maze(size, line);
+		out.print(test);
 	}
 }
--- APCSA/src/Unit14/Maze.java
+++ APCSA/src/Unit14/Maze.java
@@ -9,36 +9,133 @@ package Unit14;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
 
 public class Maze
 {
    private int[][] maze;
+   private int[][]places;
 
 	public Maze()
 	{
-
+		maze = new int[2][2]; 
+		places= new int[2][2];
 
 	}
 
 	public Maze(int size, String line)
 	{
-
-
+		maze = new int[size][size];
+		places= new int[size][size];
+		char[] c = line.toCharArray();
+		int index = 0;
+		int spaces = 0;
+		int mazeArr = 0;
+		for(int i = 0; i < c.length; i++)
+		{
+			if(c[i] == ' ')
+				spaces++;
+			
+		}
+		char[] spaceless = new char[c.length-spaces];
+		for(int i = 0; i < c.length; i++)
+		{
+			if(c[i] != ' ')
+			{
+				spaceless[index] = c[i];
+				index++;
+			}
+		}
+		index = 0;
+		for(int i = 0; i<size; i++)
+		{
+			for(int a = 0; a < size; a++)
+			{
+				maze[mazeArr][a] = Integer.parseInt(spaceless[(size*index)+a] + "");
+			}
+			index++;
+			if(mazeArr < size-1)
+				mazeArr++;
+		}
+		
+			
 	}
+		
+		
+		
+	
 
 	public boolean hasExitPath(int r, int c)
 	{
-
-		return false;
+		int size = maze[0].length;
+		boolean yn = false;
+		if(maze[r][c] == 1 && places[r][c] != 1)
+		{
+			
+			places[r][c] = 1;
+			if(r-1 >= 0)
+			{
+				if(hasExitPath(r-1, c))
+					yn = true;
+			}
+			if(c-1 >= 0)
+			{
+				if(hasExitPath(r, c-1))
+					yn = true;
+			}
+			
+			if(r-1>=0 && c+1 <size)
+			{
+				if(hasExitPath(r-1, c+1))
+					yn = true;
+			}
+			
+			if(r+1<size && c-1>=0)
+			{
+				if(hasExitPath(r+1, c-1))
+					yn = true;
+			}
+			if(c+1 <size)
+			{
+				if(hasExitPath(r, c+1))
+					yn = true;
+			}
+			if(r+1<size)
+			{
+				if(hasExitPath(r+1, c))
+					yn = true;
+			}
+			
+			out.println(yn);
+		}
+		else
+			yn = false;
+		return yn;
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
+		
+		int index = 0;
+		for(int i = 0; i < maze[0].length * maze[0].length; i++)
+		{
+			output = output + maze[index][i%5];
+			if((i+1)%maze[0].length == 0)
+			{
+				index++;
+				output = output +"\n";
+			}
+		}
+		
+		if(hasExitPath(0,0)==true)
+		{
+			output = output + "\nexit found";
+		}
+		else
+			output = output + "\nexit not found";
 
 
 
